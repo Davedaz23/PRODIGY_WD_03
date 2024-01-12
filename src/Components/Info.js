0bac7f1d862edb186bcdd52ff2b37d49b6942e0b
Defar Gobeze Wondafrash <DefarGobeze@cbe.com.et>
2024-01-12 18:19:00 +0300

    First commit

---
+++ src/Components/Info.js
@@ -0,0 +1,12 @@
+import "./css/info.css";
+ 
+const Info = () => {
+    return (
+        <div className="info">
+            <div className="player">Defar: X</div>
+            <div className="player">Player 1: O</div>
+        </div>
+    );
+};
+ 
+export default Info;
